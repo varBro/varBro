04d8f7361f194249debf85c0fd48282e0179236e
Wojciech Sekta <wsekta@student.agh.edu.pl>
2020-05-13 03:24:24 +0200

    Add authentication in WebConfiguration Fix issue with login page

--- src/main/java/com/varbro/varbro/WebConfiguration.java
+++ src/main/java/com/varbro/varbro/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.varbro.varbro;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,8 +10,10 @@ public class WebConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/").hasRole("USER")
+                .and().authorizeRequests().anyRequest().hasRole("ADMIN")
+                .and().formLogin().permitAll()
+                .and().logout().permitAll();
 
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
