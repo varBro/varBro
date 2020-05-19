d6179558a0e1d358fb200209ccdf0e3124917c6e
dstopka <dariusz.marek.stopka@gmail.com>
2020-05-19 13:52:58 +0200

    Add user adding GET and POST

--- src/main/java/com/varbro/varbro/controller/AdminController.java
+++ src/main/java/com/varbro/varbro/controller/AdminController.java
@@ -1,13 +1,34 @@
 package com.varbro.varbro.controller;
 
+import com.varbro.varbro.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
 
     @GetMapping("/admin")
-    public String userIndex(){
+    public String adminIndex(){
         return "admin/index";
     }
+
+    @GetMapping("/admin/add-user")
+    public String adminAddUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "/admin/add-user";
+    }
+
+    @PostMapping("/admin/add-user")
+    public String adminAddUserSubmit(@ModelAttribute User user) {
+        return "admin/user-submited";
+    }
+
+    @GetMapping("/admin/users")
+    public String adminUsers(){
+        return "/login";
+    }
+
 }
