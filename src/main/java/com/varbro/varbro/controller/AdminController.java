dbf4785cc70862f98163ce87e6a0899c3bca399a
mprzysiezniuk <mariuszprzysiezniuk@gmail.com>
2020-05-22 17:13:34 +0200

    added random password generator

--- src/main/java/com/varbro/varbro/controller/AdminController.java
+++ src/main/java/com/varbro/varbro/controller/AdminController.java
@@ -2,6 +2,11 @@ package com.varbro.varbro.controller;
 
 import com.varbro.varbro.model.User;
 import com.varbro.varbro.repository.UserRepository;
+import org.junit.Test;
+import org.passay.CharacterData;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
 
 @Controller
 public class AdminController {
@@ -29,7 +36,7 @@ public class AdminController {
 
     @PostMapping("/admin/add-user")
     public ModelAndView adminAddUserSubmit(@ModelAttribute User user) {
-        user.setPassword("abc");
+        user.setPassword(generatePassayPassword());
         userRepository.save(user);
         return new ModelAndView("redirect:/user/" + user.getUserId());
     }
@@ -37,6 +44,40 @@ public class AdminController {
     @GetMapping("/admin/users")
     public String adminUsers(){
         return "/login";
+    }
+
+
+    public String generatePassayPassword() {
+        PasswordGenerator gen = new PasswordGenerator();
+        CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+        CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+        lowerCaseRule.setNumberOfCharacters(2);
+
+        CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+        CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+        upperCaseRule.setNumberOfCharacters(2);
+
+        CharacterData digitChars = EnglishCharacterData.Digit;
+        CharacterRule digitRule = new CharacterRule(digitChars);
+        digitRule.setNumberOfCharacters(2);
+
+        CharacterData specialChars = new CharacterData() {
+
+            @Override
+            public String getErrorCode() {
+                return null;
+            }
+
+            public String getCharacters() {
+                return "!@#$%^&*()_+";
+            }
+        };
+        CharacterRule splCharRule = new CharacterRule(specialChars);
+        splCharRule.setNumberOfCharacters(2);
+
+        String password = gen.generatePassword(10, splCharRule, lowerCaseRule,
+                upperCaseRule, digitRule);
+        return password;
     }
 
 }
