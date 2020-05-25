7acedcbb2717954c6ef8c4fb70e644f93b418432
dstopka <dariusz.marek.stopka@gmail.com>
2020-05-25 13:44:55 +0200

    Add user editing by admin

--- src/main/java/com/varbro/varbro/controller/UserController.java
+++ src/main/java/com/varbro/varbro/controller/UserController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -20,7 +23,7 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}")
-    public String show(@PathVariable("id") long id, Model model)
+    public String showUser(@PathVariable("id") long id, Model model)
     {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
@@ -29,10 +32,20 @@ public class UserController {
         return "user/profile";
     }
 
-    @GetMapping("/users")
-    public String showAll(Model model)
+    @GetMapping("/user/{id}/edit")
+    public String showEditForm(@PathVariable("id") long id, Model model)
     {
-        model.addAttribute("users", userRepository.findAll());
-        return "user/users";
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        model.addAttribute("user", user);
+        return "user/edit";
+    }
+
+    @PostMapping("/user/{id}/edit")
+    public ModelAndView editUser(@ModelAttribute User user)
+    {
+        userRepository.save(user);
+        return new ModelAndView("redirect:/user/" + user.getUserId());
     }
 }
